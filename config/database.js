9d4c02e641f3f191bde8cdadba48c4f6f92f3a2c
Thom Woltman <t-woltman@hotmail.com>
2019-01-11 22:05:47 +0100

    mongodb

--- config/database.js
+++ config/database.js
@@ -1,5 +1,7 @@
 module.exports = {
 
-    'url' : 'mongodb://localhost:27017/restrace' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
+    //'url' : 'mongodb://localhost:27017/restrace' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    //'url' : 'mongodb://restrace:acLNfMWB6JaNZqKg@cluster0-rkeks.mongodb.net/admin'
+    
+    'url' : 'mongodb://restrace:acLNfMWB6JaNZqKg@cluster0-shard-00-00-rkeks.mongodb.net:27017,cluster0-shard-00-01-rkeks.mongodb.net:27017,cluster0-shard-00-02-rkeks.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
 };
